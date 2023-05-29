57a7c66eec3667e0bf7f3b06b839c6dfbebfa2e6
Derly MOUPEPIDI <moupepidid@gmail.com>
2023-05-29 01:06:44 +0100

    creation page newsletter popup

---
+++ Web.Buyify/Pages/Shared/_NewsletterPopup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Buyify.Pages.Shared
+{
+    public class _NewsletterPopupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
